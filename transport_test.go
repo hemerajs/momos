202e0e8ab99685d553c45f1b03d9030f8280646e
Dustin Deus <deusdustin@gmail.com>
2017-08-20 18:08:16 +0200

    create test suite

--- transport_test.go
+++ transport_test.go
@@ -6,64 +6,95 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var s struct {
+	server *httptest.Server
+	proxy  *httptest.Server
+	client http.Client
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func setup() {
+
+	mux := http.NewServeMux()
+	s.server = httptest.NewServer(mux)
+	s.client = http.Client{}
+
+	mux.HandleFunc("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "max-age=10")
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `
+			<ssi name="basket" timeout="200" template="true" src="`+s.server.URL+`/ssi">Default content!	
+			<ssi-timeout>
+			<span>Please try it again!</span>
+			</ssi-timeout>
+			<ssi-error>
+			<span>Please call the support!</span>
+			</ssi-error>
+		</ssi>
+		`)
+	}))
+
+	mux.HandleFunc("/api/ssi500", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "max-age=10")
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `
+			<ssi name="basket" timeout="200" template="true" src="`+s.server.URL+`/ssi500">Default content!	
+			<ssi-timeout>
+			<span>Please try it again!</span>
+			</ssi-timeout>
+			<ssi-error>
+			<span>Please call the support!</span>
+			</ssi-error>
+		</ssi>
+		`)
+	}))
+
+	p := New(s.server.URL)
+	s.proxy = httptest.NewServer(p.Handler)
+
+	mux.HandleFunc("/ssi", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "max-age=10")
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `<h1>hello</h1>`)
+	}))
+
+	mux.HandleFunc("/ssiTimeout", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1000 * time.Millisecond)
+		w.Header().Add("Cache-Control", "max-age=10")
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `<h1>hello</h1>`)
+	}))
+
+	mux.HandleFunc("/ssi500", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Cache-Control", "max-age=10")
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `<h1>hello</h1>`)
+	}))
+}
+
+func teardown() {
+	s.server.Close()
+	s.proxy.Close()
+}
+
 var ssiURL string
 
-func SSITimeoutHandler(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(1000 * time.Millisecond)
-	w.Header().Add("Cache-Control", "max-age=10")
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `<h1>hello</h1>`)
-}
-
-func SSIInvalidStausCodeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Header().Add("Cache-Control", "max-age=10")
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `<h1>hello</h1>`)
-}
-
-func SSIHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Cache-Control", "max-age=10")
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `<h1>hello</h1>`)
-}
-
-func APIHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Cache-Control", "max-age=10")
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `
-		<ssi name="basket" timeout="200" template="true" src="`+ssiURL+`">Default content!	
-		<ssi-timeout>
-		<span>Please try it again!</span>
-		</ssi-timeout>
-		<ssi-error>
-		<span>Please call the support!</span>
-		</ssi-error>
-	</ssi>
-	`)
-}
-
 func TestSSIContent(t *testing.T) {
-
-	ssiServer := httptest.NewServer(http.HandlerFunc(SSIHandler))
-	ssiURL = ssiServer.URL
-	defer ssiServer.Close()
-
-	tsAPI := httptest.NewServer(http.HandlerFunc(APIHandler))
-	defer tsAPI.Close()
-
-	p := New(tsAPI.URL)
-
-	server := httptest.NewServer(p.Handler)
-	defer server.Close()
-
-	res, err := http.Get(server.URL)
+	res, err := s.client.Get(s.proxy.URL + "/api")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,5 +103,18 @@ func TestSSIContent(t *testing.T) {
 	bodyString := string(body)
 
 	assert.Equal(t, res.StatusCode, 200, "should return 200")
-	assert.Equal(t, "<html><head></head><body><h1>hello</h1>\n\t</body></html>", bodyString)
+	assert.Equal(t, "<html><head></head><body><h1>hello</h1>\n\t\t</body></html>", bodyString)
+}
+
+func TestError(t *testing.T) {
+	res, err := s.client.Get(s.proxy.URL + "/api/ssi500")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+	bodyString := string(body)
+
+	assert.Equal(t, res.StatusCode, 200, "should return 200")
+	assert.Equal(t, "<html><head></head><body>\n\t\t\t<span>Please call the support!</span>\n\t\t\t\n\t\t</body></html>", bodyString)
 }
